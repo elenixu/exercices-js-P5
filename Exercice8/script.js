0a6b6a4c0e4ca1d55b0ecf238254fa696ea05de9
Elena <elegil93@gmail.com>
2024-11-22 09:24:18 +0100

    Finished Excercises

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,7 @@
 //Votre code ici
 
- export default add;
+function add(a, b) {
+  return a + b
+}
+console.log(add(1, 2))
+export default add
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,16 @@
 // votre code ici
+function checkAge(age) {
+  if (age < 18) {
+    return 'Vous êtes mineur.'
+  } else if (age >= 18 && age <= 65) {
+    return 'Vous êtes majeur.'
+  } else if (age > 65) {
+    return 'Vous êtes senior.'
+  }
+}
 
-export default checkAge;
+console.log(checkAge(15))
+console.log(checkAge(30))
+console.log(checkAge(75))
+
+export default checkAge
